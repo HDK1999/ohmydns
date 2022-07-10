54d1edb0fc7396e89e02e1a3aaa8865aa7e1ced2
HDK1999 <gg19990815@gmail.com>
2022-07-10 21:48:14 +0800

    新增定时清除系统缓存日志功能

--- src/main.go
+++ src/main.go
@@ -3,25 +3,44 @@ package main
 // ohmydns入口文件
 
 import (
+	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"net"
 	Dns "ohmydns/src/dns"
 	"ohmydns/src/util"
+	"reflect"
 	"strconv"
+	"time"
 )
+
+// 定时任务
+func exejob(t time.Ticker) {
+	for {
+		select {
+		case <-t.C:
+			fmt.Println("begin clean")
+			fmt.Println(util.RLog.NumLog2Str("111"))
+			fmt.Println(reflect.TypeOf(util.RLog))
+			util.NewResolverLog()
+		}
+	}
+}
 
 func main() {
 	// 初始化日志工具
 	util.Initlogger("./log/main.log")
 	// 初始化实验记录缓冲区
 	util.NewResolverLog()
-	// 初始化IP记录缓冲区
-	util.NewIPLog()
+	// 初始化IP记录缓冲区，已废除
+	//util.NewIPLog()
+	// 初始化计时器，用于定时清空变量空间，减小系统负担
+	t := time.NewTicker(time.Hour * 48)
+	go exejob(*t)
 
 	//Listen on UDP Port at ipv4&ipv6
 	Serveaddr := net.UDPAddr{
-		Port: 1153,
+		Port: 53,
 		IP:   net.ParseIP("localhost"),
 	}
 	//ipv4和ipv6解析
